3941b754a1dddf6087a9872ba18d221f13185b3a
Stefano Orlando <stefanoorlando@MBP-di-Stefano.routerone>
2018-03-07 16:44:56 +0100

    Better layout with FlexView

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,62 +1,20 @@
-/*
-
-This component is the entry point for our app.
-It must be named exactly `App` and live in the `components/App` folder.
-Typical tasks performed in this component are:
-- general app layout
-- choosing the correct component to render based on the current view
-
-In this simple example it does a bit of both.
-
-*/
-
 import * as React from 'react';
 import View from 'View';
 
-import { declareQueries } from '@buildo/bento/data';
-import { CurrentView } from 'model';
-
 import './app.scss';
 
-class App extends React.Component<{ currentView: CurrentView }> {
+export default class App extends React.Component {
   render() {
     return (
       <View column className="main">
-        <div className="header">
+        <View basis={50} className="header" vAlignContent="center">
           <h1>header</h1>
-        </div>
+        </View>
 
-        <View column className="content">
-          <View column className="results">
-            <h1 className="placeholder">Search in the top</h1>
-            <h1 className="placeholder">Search in the top</h1>
-            <h1 className="placeholder">Search in the top</h1>
-            <h1 className="placeholder">Search in the top</h1>
-            <h1 className="placeholder">Search in the top</h1>
-            <h1 className="placeholder">Search in the top</h1>
-            <h1 className="placeholder">Search in the top</h1>
-            <h1 className="placeholder">Search in the top</h1>
-            <h1 className="placeholder">Search in the top</h1>
-            <h1 className="placeholder">Search in the top</h1>
-            <h1 className="placeholder">Search in the top</h1>
-            <h1 className="placeholder">Search in the top</h1>
-            <h1 className="placeholder">Search in the top</h1>
-            <h1 className="placeholder">Search in the top</h1>
-            <h1 className="placeholder">Search in the top</h1>
-            <h1 className="placeholder">Search in the top</h1>
-            <h1 className="placeholder">Search in the top</h1>
-            <h1 className="placeholder">Search in the top</h1>
-            <h1 className="placeholder">Search in the top</h1>
-            <h1 className="placeholder">Search in the top</h1>
-            <h1 className="placeholder">Search in the top</h1>
-            <h1 className="placeholder">Search in the top</h1>
-            <h1 className="placeholder">Search in the top</h1>
-          </View>
+        <View column className="content" grow hAlignContent="center">
+          <View column className="results" grow width="100%" />
         </View>
       </View>
     );
   }
 }
-
-const queries = declareQueries(['currentView']);
-export default queries(App);
