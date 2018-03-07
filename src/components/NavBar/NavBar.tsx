adb09ad9a2a50f2333606fe4fe4d43607bb57519
Stefano Orlando <stefanoorlando@MBP-di-Stefano.routerone>
2018-03-07 16:24:53 +0100

    Implemented NavBar component

---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import NavBarBRC from 'NavBarBRC';
+import SearchBar from 'SearchBar';
+
+import './navBar.scss';
+const left = () => {
+  return <h1 className="header-name">Ste-gh-explorer</h1>;
+};
+
+export default class NavBar extends React.Component {
+  onSearchSubmit = (query: string) => {
+    alert(query);
+  };
+
+  render() {
+    return (
+      <NavBarBRC
+        className="header-content"
+        content={{
+          left: left(),
+          center: <SearchBar onSearchSubmit={this.onSearchSubmit} />
+        }}
+      />
+    );
+  }
+}
