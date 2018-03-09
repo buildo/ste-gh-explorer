279a74ba30e677876b308a3d3950b25fc8169cc4
Stefano Orlando <stefanoorlando@protonmail.com>
2018-03-09 10:34:24 +0100

    Internationalization of AppName in NavBar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,10 +1,15 @@
 import * as React from 'react';
 import NavBarBRC from 'NavBarBRC';
 import SearchBar from 'SearchBar';
+import { FormattedMessage } from 'react-intl';
 
 import './navBar.scss';
 const left = () => {
-  return <h1 className="nav-bar-header-name">GH-Explorer</h1>;
+  return (
+    <h1 className="nav-bar-header-name">
+      <FormattedMessage id="NavBar.appName" />
+    </h1>
+  );
 };
 
 export default class NavBar extends React.Component {
