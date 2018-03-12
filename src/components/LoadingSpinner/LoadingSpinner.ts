3c32f528ad93cef5a99fda8e488df592a8453442
Stefano Orlando <stefanoorlando@protonmail.com>
2018-03-12 16:13:03 +0100

    Fix import LoadingSpinner scss

--- src/components/LoadingSpinner/LoadingSpinner.ts
+++ src/components/LoadingSpinner/LoadingSpinner.ts
@@ -1,4 +1,4 @@
 import LoadingSpinner from '@buildo/bento/components/LoadingSpinner';
-import '@buildo/bento/components/loadingspinner.scss';
+import '@buildo/bento/components/loadingSpinner.scss';
 
 export default LoadingSpinner;
