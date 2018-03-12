ce483a5bb397b1ef02ce84b03fa8dd87de706b89
Stefano Orlando <stefanoorlando@protonmail.com>
2018-03-12 12:19:35 +0100

    Add API request to github

--- src/API/API.ts
+++ src/API/API.ts
@@ -1,5 +1,16 @@
+const API_URL = 'https://api.github.com/';
+const SEARCH_REPO_ENDPOINT = 'search/repositories';
+
 export const getRandomName = (length: number) => {
-  return fetch(`http://uinames.com/api/?minlen=${length}&maxlen=${length}`).then(res => res.json()).then(res => {
-    return `${res.name} ${res.surname}`;
-  }) as Promise<string>;
+  return fetch(`http://uinames.com/api/?minlen=${length}&maxlen=${length}`)
+    .then(res => res.json())
+    .then(res => {
+      return `${res.name} ${res.surname}`;
+    }) as Promise<string>;
+};
+
+export const searchGithubRepo = (query: string) => {
+  return fetch(API_URL + SEARCH_REPO_ENDPOINT + `?q=${query}`).then(res =>
+    res.json()
+  ) as Promise<Array<Object>>;
 };
