545beb9d5278a92e86fea4a782555731ac514af6
Stefano Orlando <stefanoorlando@protonmail.com>
2018-03-13 18:24:15 +0100

    Update App

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,22 +2,55 @@ import * as React from 'react';
 import View from 'View';
 import NavBar from 'NavBar';
 import ResultsPanel from 'ResultsPanel';
+import ResultDetails from 'ResultDetails';
+
 import './app.scss';
+import { declareQueries, declareCommands } from '@buildo/bento/data';
+import { CurrentView } from 'model';
 
 type State = {
   query: string;
 };
-export default class App extends React.Component<{}, State> {
-  state = {
+type Props = {
+  currentView: CurrentView;
+  doUpdateLocation(location: {
+    pathname: string | undefined;
+    search: string | undefined;
+  }): void;
+};
+
+class App extends React.Component<Props, State> {
+  state: State = {
     query: ''
   };
   onSearchSubmit = (query: string) => {
     this.setState({ query });
   };
+
+  onDialogClose = () => {
+    this.props.doUpdateLocation({ pathname: '', search: 'r' });
+  };
+
+  renderModal = () => {
+    const { currentView } = this.props;
+    if (currentView && currentView.payload.repo) {
+      return (
+        <ResultDetails
+          query={this.state.query}
+          ID={currentView.payload.repo}
+          onDialogClose={this.onDialogClose}
+        />
+      );
+    }
+    return null;
+  };
+
   render() {
-    const { state: { query }, onSearchSubmit } = this;
+    const { state, onSearchSubmit, renderModal } = this;
+
     return (
       <View column className="app">
+        {renderModal()}
         <View
           basis={50}
           className="app-header"
@@ -28,9 +61,14 @@ export default class App extends React.Component<{}, State> {
         </View>
 
         <View column className="app-content" grow hAlignContent="center">
-          <ResultsPanel query={query} />
+          <ResultsPanel query={state.query} />
         </View>
       </View>
     );
   }
 }
+
+const queries = declareQueries(['currentView']);
+const command = declareCommands(['doUpdateLocation']);
+
+export default command(queries(App));
